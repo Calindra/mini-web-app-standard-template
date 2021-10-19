b5ddf34603690455a53c1e8f315a51a4d7defac4
Thiago <thiago.rodrigues@calindra.com.br>
2021-10-19 15:27:24 -0300

    adicionando boilerplate 2.0 inicial

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,5 @@
 <Window>
 	<View>
-    <Illustration height={200} image={require('../assets/images/icon_ame.svg')}/>
-    <Header textAlign="center">Boilerplate Mini App</Header>
-    <Spacing size="lg" />
-    <ListView items={this.state.links} onItemSelect={this.navigateTo} />
+    <Text>Hello world!</Text>
   </View>
 </Window>
--- src/views/Servless.jsx
+++
@@ -1,6 +0,0 @@
-<Window>
-  <View>
-    <Illustration height={200} image={require('../assets/images/icon_ame.svg')}/>
-    <Header textAlign="center" >{this.state.helloMessage}</Header>
-  </View>
-</Window>
